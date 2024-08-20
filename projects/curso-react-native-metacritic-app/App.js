8d8fd5b2ccbff4cfb33fd101d1abefebdff3d7af
sariza <sariza@intelix.biz>
2024-08-20 09:20:51 -0400

    Subiendo Curso React Native Metacritic

---
+++ projects/curso-react-native-metacritic-app/App.js
@@ -0,0 +1,48 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
+import { getLatestGames } from './lib/metacritic';
+
+export default function App() {
+
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    getLatestGames().then(games => {
+      setGames(games);
+    });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+
+      {
+        games.map(game => {
+          <View key={game.slug} style={styles.card}>
+            <Image
+             source={{ uri: game.image }}
+             style={{
+              width: 107,
+              height: 147,
+              borderRadius: 10,
+             }}
+             >
+
+             </Image>
+          </View>
+        })
+      }
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
